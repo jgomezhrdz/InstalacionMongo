de6968dcde91137a097ca9a0d8b4573282236e8e
juangomezhrdz <juan.gomez338@comunidadunir.net>
2022-01-24 19:15:35 +0100

    v1

--- instalacionMongo.sh
+++ instalacionMongo.sh
@@ -4,12 +4,12 @@ logger "Arrancando instalacion y configuracion de MongoDB"
 USO="Uso : install.sh [opciones]
 Ejemplo:
 install.sh -u administrador -p password [-n 27017]
- Opciones:
-    -f archivo de configuración
-    -u usuario
-    -p password
-    -n numero de puerto (opcional)
-    -a muestra esta ayuda
+Opciones:
+-f archivo de configuración
+-u usuario
+-p password
+-n numero de puerto (opcional)
+-a muestra esta ayuda
 "
 
 function ayuda() {
@@ -28,31 +28,45 @@ do
         p ) PASSWORD=$OPTARG
            echo "Parametro PASSWORD establecido";;
         n ) PUERTO_MONGOD=$OPTARG
-           echo "Parametro PUERTO_MONGOD establecido con '${PUERTO_MONGOD}'";; 
+           echo "Parametro PUERTO_MONGOD establecido con '${PUERTO_MONGOD}'";;
         f ) ARCHIVO = $OPTARG
         a ) ayuda; exit 0;;
         : ) ayuda "Falta el parametro para -$OPTARG"; exit 1;; \?) ayuda "La opcion no existe : $OPTARG"; exit 1;;
     esac
 done
-if [[ -z ${ARCHIVO} ]]; then 
-    if [ -z ${USUARIO} ]
-        then
-            ayuda "El usuario (-u) debe ser especificado"; exit 1
-        fi
-    if [ -z ${PASSWORD} ] 
-        then
-            echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb.list
-        fi
-    if [ -z ${PUERTO_MONGOD} ]
-        then
-            PUERTO_MONGOD=27017
-        fi
-else
+#Comprobar las variables del archivo de configuración
+if [[ ! -z ${ARCHIVO} ]]; then
     if [[ -f ${ARCHIVO} ]]; then
-    
+        extension="${ARCHIVO##*.}"
+        if [[ extension = "ini" ]]; then
+            source $ARCHIVO
+            if [[ ! -z ${user}]]; then 
+                USUARIO = $user
+            fi
+            if [[ ! -z ${password}]]; then 
+                PASSWORD = $password
+            fi
+            if [[ ! -z ${port} ]]; then 
+                PORT = $port 
+            fi
+        else
+            echo "el archivo de configuración no tiene el formato correcto"
+        fi
     else
-        echo "el archivo de configuración no tiene el formato correcto"  
+        echo "el archivo de configuración es incorrecto"
     fi
+fi
+if [ -z ${USUARIO} ]
+    then
+        ayuda "El usuario (-u) debe ser especificado"; exit 1
+fi
+if [ -z ${PASSWORD} ]
+    then
+        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb.list
+fi
+if [ -z ${PUERTO_MONGOD} ] 
+    then
+        PUERTO_MONGOD=27017
 fi
 
 # Preparar el repositorio (apt-get) de mongodb añadir su clave apt
@@ -72,7 +86,7 @@ if [[ -z "$(mongo --version 2> /dev/null | grep '4.2.1')" ]]
         && rm -rf /var/lib/apt/lists/* \
         && pkill -u mongodb || true \
         && pkill -f mongod || true \
-        && rm -rf /var/lib/mongodb 
+        && rm -rf /var/lib/mongodb
 fi
 # Crear las carpetas de logs y datos con sus permisos
 [[ -d "/datos/bd" ]] || mkdir -p -m 755 "/datos/bd"
